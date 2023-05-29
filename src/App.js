30e0d5b63b0c63b693898056575553011b8d2a19
HangerIsACat <sheenadale.mariano@gmail.com>
2023-05-29 21:55:33 +0800

    Move code for fetching locations to LocationAPI.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
 
 import React, { useState, useEffect } from "react";
 
-import LocationAPI from "./apis/LocationAPI.js"
-
 import LocationModule from "./module/LocationModule.js";
 
 function App() {
 
-  const [locations, setLocations] = useState([]);
-  const [currentLocation, setCurrentLocation] = useState([]);
-
-  const locationAPI = new LocationAPI();
-
-  useEffect(() => {
-    locationAPI.getAllLocations(setLocations);
-  }, []);
-
   return (
     <div>
-      <LocationModule id="location-module" locations = { locations } api={ locationAPI } />
+      <LocationModule id="location-module" />
     </div>
   );
 };
