27a6fbf9eef31236f842521b0f7b9bcd76f17821
HangerIsACat <sheenadale.mariano@gmail.com>
2023-05-29 21:59:00 +0800

    Change way of calling callback for LocationAPI.getAll()

--- src/apis/LocationAPI.js
+++ src/apis/LocationAPI.js
@@ -3,18 +3,27 @@ export default class LocationAPI {
 
   constructor() {  };
 
-  async getAllLocations(callback) {
+  async getAll(callback) {
     try {
       const response = await fetch("http://localhost:3001/locations");
       const data = await response.json();
 
-      callback(Array.from(data));
+      if (callback) {
+        callback(data);
+      }
     } catch (error) {
       console.error(error);
     }
   };
 
-  async deleteLocation(location, callback) {
+  add(locParent) {
+    console.log(`Add location to ${locParent.name} : ${locParent.id}`);
+  }
+  edit(location) {
+    console.log(`Edit location ${location.name}`);
+  }
+
+  async delete(location, callback) {
     try {
 
       const response = await fetch(
