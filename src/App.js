570e3985449ceb752574d764901b8a794d0f6e5d
HangerIsACat <sheenadale.mariano@gmail.com>
2023-05-30 22:51:11 +0800

    Add book module in App.js

--- src/App.js
+++ src/App.js
@@ -2,14 +2,15 @@
 import React, { useState, useEffect } from "react";
 
 import LocationModule from "./module/LocationModule.js";
+import BookModule from "./module/BookModule.js"
 
 function App() {
-
+  
   return (
     <div>
-      {/* <h2>Records of Books and Their Locations</h2> */}
-      <h2>Locations</h2>
+      <h2>Records of Books and Their Locations</h2>
       <LocationModule id="location-module" />
+      <BookModule id="book-module" />
     </div>
   );
 };
