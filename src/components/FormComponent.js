2900594ae5c95652bdd2dbb249d1c9e95740f24a
HangerIsACat <sheenadale.mariano@gmail.com>
2023-05-30 15:04:49 +0800

    Move handlers to LocationModule component and Dialog component to FormDialogComponent

--- src/components/FormComponent.js
+++ src/components/FormComponent.js
@@ -6,48 +6,20 @@ import ButtonGroup from "@mui/material/ButtonGroup";
 import Paper from "@mui/material/Paper";
 import InputBase from "@mui/material/InputBase";
 import InputLabel from "@mui/material/InputLabel";
-import { Dialog, DialogActions, DialogContent, DialogTitle } from "@mui/material";
 
 function FormComponent({ 
   frmLabel, 
-  valuesAll,
-  valueFocused, 
-  valueFocusedTxt, 
   valueTxt, 
-  api, 
-  id }) {
-
-  const [inputValue, setInputValue] = useState("");
-  const [open, setOpen] = useState(false);
-
-  useEffect(() => {
-    setInputValue(valueTxt);
-  });
-
-  function openDialog() {
-    setOpen(true);
-  }
-
-  function closeDialog() {
-    setOpen(false);
-  }
-
-  function add(text) {
-    console.log(`Add text: ${text}`);
-    // api.add(text);
-  }
-
-  function edit(text) {
-    console.log(`Edit text: ${text}`);
-    // api.edit(text);
-  }
-
-  function deleteValue() {
-    api.delete(valueFocused);
-  }
+  id, 
+  handlerAdd, 
+  handlerEdit, 
+  handlerDelete, 
+  handlerFocus, 
+  handlerBlur, 
+  handlerChange 
+}) {
 
   return (
-    <div>
     <Paper id={ id } component="form" 
       sx={{ 
         p: "2px 4px", 
@@ -62,31 +34,19 @@ function FormComponent({
       <InputBase 
         id="frm-text" 
         label={ frmLabel } 
-        value={ inputValue } 
+        value={ valueTxt } 
         variant="outlined" 
         sx={{ ml: 1, flex: 1, position: "relative" }} 
+        onFocus={ handlerFocus } 
+        onBlur={ handlerBlur } 
+        onChange={ handlerChange } 
       />
       <ButtonGroup id="frm-btngrp" disableElevation variant="contained">
-        <Button id="frm-btn-add" onClick={ openDialog } >Add</Button>
-        <Button id="frm-btn-edit" onClick={ openDialog } >Edit</Button>
-        <Button id="frm-btn-delete" onClick={ deleteValue } >Delete</Button>
+        <Button id="frm-btn-add" onClick={ handlerAdd } >Add</Button>
+        <Button id="frm-btn-edit" onClick={ handlerEdit } >Edit</Button>
+        <Button id="frm-btn-delete" onClick={ handlerDelete } >Delete</Button>
       </ButtonGroup> 
     </Paper>
-
-    <Dialog open={ open } onClose={ closeDialog }>
-      <DialogTitle>{ frmLabel }</DialogTitle>
-      <DialogContent>
-        <InputBase 
-          autoFocus 
-          value={ inputValue } 
-          onChange={(e) => { setInputValue(e.target.value) }} />
-      </DialogContent>
-      <DialogActions>
-        <Button id="frm-btn-ok" onClick={() => { add(inputValue) }} >Ok</Button>
-        <Button id="frm-btn-cancel" onClick={ closeDialog } >Cancel</Button>
-      </DialogActions>
-    </Dialog>
-    </div>
   );
 
 }
